a228af53eed6d511835c0fb9a0b0003204f991f8
Vladimir <jidkov91@gmail.com>
2024-01-07 21:00:05 +0300

    6 done

---
+++ oop/homework/Main.java
@@ -0,0 +1,13 @@
+package homework;
+
+
+
+public class Main{
+    public static void main(String[] args){
+        User user = new User("Bob");
+        Persister pers = new Persister(user);
+		Report report = new Report();
+        report.report(user);
+        pers.save();
+    }
+}
